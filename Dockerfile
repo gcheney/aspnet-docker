ed55b358520610318481b68f8e85221c25b3d949
Glendon Cheney <glendon.cheney@gmail.com>
2017-01-09 19:56:24 -0600

    Add configuration of environment variables

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
-FROM microsoft/dotnet:latest
+FROM microsoft/dotnet:1.0.1-sdk-projectjson
 
 COPY . /app
-
 WORKDIR /app
 
 RUN ["dotnet", "restore"]
-
 RUN ["dotnet", "build"]
 
 EXPOSE 5000/tcp
+ENV ASPNETCORE_URLS http://*:5000
 
-CMD ["dotnet", "run", "--server.urls", "http://*:5000"]
+ENTRYPOINT ["dotnet", "run"]
--- Startup.cs
+++ Startup.cs
@@ -4,11 +4,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace AspNetDocker
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+            Environment = env;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -26,7 +42,7 @@ namespace AspNetDocker
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"Hello, World! It is { DateTime.Now }");
+                await context.Response.WriteAsync($"Hello, World! The message is { Configuration["Message"] }");
             });
         }
     }
